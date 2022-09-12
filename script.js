d53abfc3365ba0744bfdb7a7577c8cfcbf65e40b
jessymatta <jessy.w.matta@gmail.com>
2022-09-12 19:20:09 +0300

    added a function to extract inputs from the contact me section

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+// Main function
+function main(){
+    let full_name=document.getElementById("full_name").value;
+    console.log(full_name);
+
+    let email=document.getElementById("email_address").value;
+    console.log(email);
+
+    let phone_nbr=document.getElementById("phone_nb").value;
+    console.log(phone_nbr);
+
+    let msg=document.getElementById("msg").value;
+    console.log(msg);
+
+
+}
